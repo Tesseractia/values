50467d61403832f82dc44f0829f3239ad272f520
Tesseractia <147262500+Tesseractia@users.noreply.github.com>
2023-10-07 17:18:27 +0100

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,119 +1,128 @@
 const ideologies = { // eslint-disable-line no-unused-vars
-  'Marxism-Leninism': {
+  'lyfon justifier': {
+    a: 0,
+    b: 0,
+    c: 75,
+    d: 70,
+    e: 50,
+    f: 90,
+    g: 55
+  },
+  'Walter justifier': {
     a: 80,
     b: 80,
-    c: 80,
+    c: 0,
+    d: 20,
+    e: 50,
+    f: 100,
+    g: 65
+  },
+  'Eloy justifier': {
+    a: 100,
+    b: 100,
+    c: 20,
+    d: 0,
+    e: 50,
+    f: 30,
+    g: 50
+  },
+  'Oliver justifier': {
+    a: 0,
+    b: 60,
+    c: 60,
+    d: 50,
+    e: 60,
+    f: 25,
+    g: 0
+  },
+  'Extreme Pacifism': {
+    a: 0,
+    b: 65,
+    c: 65,
+    d: 65,
+    e: 50,
+    f: 25,
+    g: 45
+  },
+  'Tetradimensionalism': {
+    a: 100,
+    b: 40,
+    c: 0,
+    d: 90,
+    e: 100,
+    f: 50,
+    g: 100
+  },
+  'Adimensionalism': {
+    a: 100,
+    b: 50,
+    c: 100,
+    d: 70,
+    e: 50,
+    f: 0,
+    g: 40
+  },
+  'Lyfoncide': {
+    a: 80,
+    b: 100,
+    c: 60,
+    d: 40,
+    e: 70,
+    f: 50,
+    g: 80
+  },
+  'Waltercide': {
+    a: 80,
+    b: 0,
+    c: 100,
+    d: 100,
+    e: 10,
+    f: 20,
+    g: 40
+  },
+  'Eloycide': {
+    a: 80,
+    b: 0,
+    c: 100,
+    d: 100,
+    e: 50,
+    f: 50,
+    g: 60
+  },
+  'Olivercide': {
+    a: 90,
+    b: 20,
+    c: 0,
+    d: 70,
+    e: 50,
+    f: 50,
+    g: 100
+  },
+  'Equality': {
+    a: 50,
+    b: 50,
+    c: 50,
+    d: 50,
+    e: 50,
+    f: 50,
+    g: 50
+  },
+  'ProHuman': {
+    a: 50,
+    b: 60,
+    c: 40,
     d: 60,
-    e: 90,
+    e: 85,
     f: 70,
     g: 40
   },
-  'Orthodox-Marxism': {
-    a: 80,
-    b: 80,
-    c: 60,
-    d: 80,
-    e: 70,
-    f: 50,
-    g: 30
-  },
-  'Eco-Marxism': {
-    a: 80,
-    b: 80,
-    c: 60,
-    d: 80,
-    e: 70,
-    f: 20,
-    g: 30
-  },
-  'Centrist-Marxism': {
+  'ProHibrids': {
     a: 50,
-    b: 80,
-    c: 60,
+    b: 60,
+    c: 40,
     d: 60,
-    e: 70,
-    f: 50,
-    g: 30
-  },
-  'Council-Communism': {
-    a: 100,
-    b: 70,
-    c: 40,
-    d: 80,
-    e: 50,
-    f: 50,
-    g: 30
-  },
-  'Left-Communism': {
-    a: 90,
-    b: 60,
-    c: 50,
-    d: 90,
-    e: 80,
-    f: 50,
-    g: 30
-  },
-  'Anarcho-Communism': {
-    a: 80,
-    b: 50,
-    c: 20,
-    d: 80,
-    e: 10,
-    f: 50,
-    g: 30
-  },
-  'Eco-Anarchism': {
-    a: 80,
-    b: 50,
-    c: 20,
-    d: 80,
-    e: 10,
-    f: 20,
-    g: 30
-  },
-  'Market-Anarchism': {
-    a: 20,
-    b: 40,
-    c: 30,
-    d: 70,
-    e: 10,
-    f: 60,
-    g: 30
-  },
-  'Utopian-Socialism': {
-    a: 10,
-    b: 10,
-    c: 40,
-    d: 20,
-    e: 50,
+    e: 15,
     f: 40,
-    g: 50
-  },
-  'Democratic-Socialism': {
-    a: 20,
-    b: 30,
-    c: 30,
-    d: 70,
-    e: 60,
-    f: 30,
-    g: 20
-  },
-  'Social-Democracy': {
-    a: 10,
-    b: 30,
-    c: 50,
-    d: 40,
-    e: 80,
-    f: 50,
-    g: 30
-  },
-  'Left-Wing-Nationalism': {
-    a: 60,
-    b: 60,
-    c: 60,
-    d: 30,
-    e: 70,
-    f: 40,
-    g: 30
+    g: 60
   }
 }
